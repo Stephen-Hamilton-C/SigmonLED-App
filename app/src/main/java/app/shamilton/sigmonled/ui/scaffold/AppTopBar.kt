b9f791230882071df62c041f8d806844eb170cd7
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-12-09 19:14:30 -0500

    Finalized implementation of action buttons
    - Using rounded buttons
    - Added separate scan button
    - Added comments to de-mystify bluetoothButtonClicked

--- app/src/main/java/app/shamilton/sigmonled/ui/scaffold/AppTopBar.kt
+++ app/src/main/java/app/shamilton/sigmonled/ui/scaffold/AppTopBar.kt
@@ -2,10 +2,7 @@ package app.shamilton.sigmonled.ui.scaffold
 
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Bluetooth
-import androidx.compose.material.icons.filled.BluetoothConnected
-import androidx.compose.material.icons.filled.BluetoothSearching
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.runtime.*
 import app.shamilton.sigmonled.core.devMan
 import com.badoo.reaktive.observable.subscribe
@@ -14,12 +11,12 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun AppTopBar() {
-    val drawerState = AppScaffold.scaffoldState.drawerState
-    val connectedIcon = Icons.Default.BluetoothConnected
-    val disconnectedIcon = Icons.Default.Bluetooth
-    val scanIcon = Icons.Default.BluetoothSearching
+    val connectedIcon = Icons.Rounded.BluetoothConnected
+    val disconnectedIcon = Icons.Rounded.Bluetooth
     var connectIcon by remember { mutableStateOf(disconnectedIcon) }
     var connectIconEnabled by remember { mutableStateOf(true) }
+    var scanButtonEnabled by remember { mutableStateOf(true) }
+
     devMan.onDeviceConnected.subscribe {
         connectIcon = connectedIcon
         connectIconEnabled = true
@@ -30,32 +27,52 @@ fun AppTopBar() {
     }
     devMan.onAttemptingConnection.subscribe { connectIconEnabled = false }
     devMan.onAttemptingDisconnect.subscribe { connectIconEnabled = false }
-    devMan.onScanningStarted.subscribe {
-        connectIconEnabled = false
-        connectIcon = scanIcon
-    }
-    devMan.onScanningStopped.subscribe {
-        connectIconEnabled = true
-        connectIcon = if(devMan.isConnected) connectedIcon else disconnectedIcon
-    }
+    devMan.onScanningStarted.subscribe { scanButtonEnabled = false }
+    devMan.onScanningStopped.subscribe { scanButtonEnabled = true }
 
     fun bluetoothButtonClicked() {
         if (devMan.isConnected) {
+            // Currently connected, disconnect
             devMan.disconnect()
         } else if (devMan.previousDevice != null) {
+            // Not connected, though we were previously connected
+            // Attempt to connect to previous device
             devMan.connect(devMan.previousDevice!!)
         } else if (devMan.discoveredDevices.isEmpty()){
+            // Not connected, no previous connections and no devices have been found yet
+
+            // Prepare events
+            devMan.onScanningStopped.take(1).subscribe {
+                connectIcon = if(devMan.isConnected) connectedIcon else disconnectedIcon
+                connectIconEnabled = !devMan.isConnecting
+            }
             devMan.onDeviceFound.take(1).subscribe {
                 devMan.stopScan()
                 devMan.connect(it)
             }
-            devMan.scan()
+            fun scanningStarted() {
+                connectIconEnabled = false
+                connectIcon = Icons.Rounded.BluetoothSearching
+            }
+
+            if(!devMan.scanning) {
+                // If no scan is currently running, initialize one
+                devMan.onScanningStarted.take(1).subscribe { scanningStarted() }
+                devMan.scan()
+            } else {
+                // User already initialized scan, we'll just wait for events
+                scanningStarted()
+            }
         } else {
+            // No previous connections, but there are discovered devices...
+            // Let's play a game of luck and connect to the first one
+            // If the user wanted to be more specific, they should've selected one from the list
             devMan.connect(devMan.discoveredDevices.first())
         }
     }
 
     fun menuButtonClicked() {
+        val drawerState = AppScaffold.scaffoldState.drawerState
         AppScaffold.scope.launch {
             if (drawerState.isClosed)
                 drawerState.open()
@@ -67,11 +84,12 @@ fun AppTopBar() {
     TopAppBar(
         navigationIcon = {
             IconButton(onClick = { menuButtonClicked() }) {
-                Icon(Icons.Default.Menu, "Menu")
+                Icon(Icons.Rounded.Menu, "Menu")
             }
         },
         title = { Text("SigmonLED") },
         actions = {
+            // Connect button
             IconButton(
                 onClick = { bluetoothButtonClicked() },
                 enabled = connectIconEnabled,
@@ -80,6 +98,17 @@ fun AppTopBar() {
                 if(!connectIconEnabled) {
                     CircularProgressIndicator()
                 }
+            }
+
+            // Scan button
+            // TODO: Only show on devices page
+            IconButton(
+                onClick = { devMan.scan() },
+                enabled = scanButtonEnabled,
+            ) {
+                Icon(Icons.Rounded.Search, "Scan")
+                if(!scanButtonEnabled)
+                    CircularProgressIndicator()
             }
         }
     )
