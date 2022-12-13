77f4dc6bf80f5b73f5a959db312940c6ecdbc423
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-12-12 19:56:18 -0500

    BluetoothExceptions are more descriptive when thrown

--- app/src/main/java/app/shamilton/sigmonled/core/bluetooth/BluetoothException.kt
+++ app/src/main/java/app/shamilton/sigmonled/core/bluetooth/BluetoothException.kt
@@ -2,8 +2,12 @@ package app.shamilton.sigmonled.core.bluetooth
 
 import android.bluetooth.BluetoothDevice
 
-abstract class BluetoothException(val errorCode: Int) : Exception()
-class BluetoothConnectionException(val device: BluetoothDevice, errorCode: Int) : BluetoothException(errorCode)
-class BluetoothDisconnectException(val device: BluetoothDevice, errorCode: Int) : BluetoothException(errorCode)
-class BluetoothWriteException(errorCode: Int) : BluetoothException(errorCode)
-class BluetoothScanException(errorCode: Int) : BluetoothException(errorCode)
+abstract class BluetoothException(override val message: String, val errorCode: Int) : Exception()
+class BluetoothConnectionException(val device: BluetoothDevice, errorCode: Int)
+    : BluetoothException("Unexpected error when attempting to connect to device ${device.address}", errorCode)
+class BluetoothDisconnectException(val device: BluetoothDevice, errorCode: Int)
+    : BluetoothException("Unexpected error when attempting to disconnect from device ${device.address}", errorCode)
+class BluetoothWriteException(errorCode: Int)
+    : BluetoothException("Unexpected error when attempting to write to connected device", errorCode)
+class BluetoothScanException(errorCode: Int)
+    : BluetoothException("Unexpected error when scanning for nearby Bluetooth devices.", errorCode)
