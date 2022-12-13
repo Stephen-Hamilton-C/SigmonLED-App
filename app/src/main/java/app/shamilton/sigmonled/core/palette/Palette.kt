f58f7c6c85e3e527fcc53ec4d99075ad900d8c27
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-12-13 13:00:50 -0500

    Removed redundant constructors

--- app/src/main/java/app/shamilton/sigmonled/core/palette/Palette.kt
+++ app/src/main/java/app/shamilton/sigmonled/core/palette/Palette.kt
@@ -12,41 +12,7 @@ data class Palette(
     private val colors: List<Color>
 ) {
 
-    /**
-     * Creates a Palette with the color provided repeating
-     */
-    constructor(color1: Color) : this(listOf(color1))
-
-    /**
-     * Creates a Palette with the colors provided in a repeating pattern
-     */
-    constructor(color1: Color, color2: Color) : this(
-        listOf(color1, color2)
-    )
-
-    /**
-     * Creates a Palette with the colors provided in a repeating pattern
-     */
-    constructor(color1: Color, color2: Color, color3: Color, color4: Color) : this(
-        listOf(color1, color2, color3, color4)
-    )
-
-    /**
-     * Creates a Palette with the colors provided in a repeating pattern
-     */
-    constructor(color1: Color, color2: Color, color3: Color, color4: Color, color5: Color, color6: Color, color7: Color, color8: Color) : this(
-        listOf(color1, color2, color3, color4, color5, color6, color7, color8)
-    )
-
-    /**
-     * Creates a Palette with the colors provided
-     */
-    constructor(color1: Color, color2: Color, color3: Color, color4: Color, color5: Color, color6: Color, color7: Color, color8: Color,
-                color9: Color, color10: Color, color11: Color, color12: Color, color13: Color, color14: Color, color15: Color, color16: Color
-    ) : this(
-        listOf(color1, color2, color3, color4, color5, color6, color7, color8,
-            color9, color10, color11, color12, color13, color14, color15, color16)
-    )
+    constructor(vararg colors: Color) : this(colors.asList())
 
     init {
         // Sanity check: A palette must have 16 colors, or be a multiple of 16
