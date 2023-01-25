2cf1156cb626190fadae609c7906c2eea45d41f0
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2023-01-25 10:57:22 -0500

    Added color presets and hex code editor

---
+++ app/src/main/java/app/shamilton/sigmonled/ui/pages/staticcolor/ColorPresetsModel.kt
@@ -0,0 +1,45 @@
+package app.shamilton.sigmonled.ui.pages.staticcolor
+
+import android.content.Context
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+import app.shamilton.sigmonled.core.PrefKeys
+import app.shamilton.sigmonled.core.color.Color
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class ColorPresetsModel : ViewModel() {
+    var savedColors: MutableList<Color> = mutableStateListOf()
+    val defaultColors = mutableListOf(
+        Color(40.0 / 360, 200.0 / 255, 1.0),
+        Color.WHITE,
+        Color.RED,
+        Color.ORANGE,
+        Color.YELLOW,
+        Color.GREEN,
+        Color.BLUE,
+        Color(127, 0, 255),
+        Color.MAGENTA,
+    )
+
+    fun save(context: Context) {
+        val colorsData = Json.encodeToString(savedColors)
+        val prefs = context.getSharedPreferences(PrefKeys.dataFile, 0)
+        prefs.edit()
+            .putString(PrefKeys.colorPresets, colorsData)
+            .apply()
+    }
+
+    fun load(context: Context): ColorPresetsModel {
+        val prefs = context.getSharedPreferences(PrefKeys.dataFile, 0)
+        val colorsData = prefs.getString(PrefKeys.colorPresets, "")
+        savedColors = if (colorsData.isNullOrEmpty())
+            mutableStateListOf()
+        else
+            Json.decodeFromString<MutableList<Color>>(colorsData).toMutableStateList()
+
+        return this
+    }
+}
--- app/src/main/java/app/shamilton/sigmonled/ui/pages/staticcolor/StaticColorPage.kt
+++ app/src/main/java/app/shamilton/sigmonled/ui/pages/staticcolor/StaticColorPage.kt
@@ -1,12 +1,18 @@
 package app.shamilton.sigmonled.ui.pages.staticcolor
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.DeleteForever
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import app.shamilton.sigmonled.core.ArduinoCommander
 import app.shamilton.sigmonled.core.color.Color
 import app.shamilton.sigmonled.ui.ColorPicker
@@ -17,7 +23,14 @@ var colorChanged = false
 @Composable
 fun StaticColorPage(modifier: Modifier, commander: ArduinoCommander) {
     var currentColor by remember { mutableStateOf(Color(255, 255, 255)) }
+    var hexInput by remember { mutableStateOf(currentColor.hex.toString()) }
     var timerCreated by remember { mutableStateOf(false) }
+    val viewModel = viewModel<ColorPresetsModel>().load(LocalContext.current)
+
+    fun setColor(color: Color) {
+        currentColor = color
+        hexInput = color.hex.toString()
+    }
 
     if(!timerCreated) {
         timerCreated = true
@@ -31,19 +44,89 @@ fun StaticColorPage(modifier: Modifier, commander: ArduinoCommander) {
 
     Column(modifier = modifier
         .padding(24.dp)
-        .padding(bottom = 132.dp)) {
-        Button(onClick = {
-            colorChanged = true
-            currentColor = Color(40.0 / 360, 200.0 / 255, 1.0)
-            println("Changed color to $currentColor")
-        }) {
-            Text("Warm")
+    ) {
+
+        // Default colors
+        Row(
+            modifier = Modifier.horizontalScroll(rememberScrollState())
+        ) {
+            viewModel.defaultColors.forEach { color ->
+                Button(
+                    onClick = { setColor(color) },
+                    colors = ButtonDefaults.buttonColors(backgroundColor = color.toAndroidColor()),
+                ) {}
+            }
         }
+
+        // Saved colors
+        val currentContext = LocalContext.current
+        var editingColors by remember { mutableStateOf(false) }
+        Row(
+            modifier = Modifier.horizontalScroll(rememberScrollState())
+        ) {
+            viewModel.savedColors.forEach { color ->
+                Button(
+                    onClick = {
+                        if(editingColors) {
+                            viewModel.savedColors.remove(color)
+                            viewModel.save(currentContext)
+
+                            if(viewModel.savedColors.isEmpty())
+                                editingColors = false
+                        } else {
+                            setColor(color)
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(backgroundColor = color.toAndroidColor()),
+                ) {
+                    if(editingColors)
+                        Icon(Icons.Rounded.DeleteForever, "Delete")
+                }
+            }
+        }
+        Row() {
+            Button(onClick = {
+                viewModel.savedColors.add(currentColor)
+                viewModel.save(currentContext)
+            }) {
+                Text("Add Current Color")
+            }
+            if(viewModel.savedColors.isNotEmpty()) {
+                Button(onClick = { editingColors = !editingColors }) {
+                    if (editingColors)
+                        Text("Stop Editing")
+                    else
+                        Text("Edit")
+                }
+            }
+        }
+
+        var hexInputError by remember { mutableStateOf(false) }
+        TextField(
+            label = { Text("HEX Code") },
+            value = hexInput,
+            onValueChange = { value ->
+                hexInput = if(!value.startsWith("#"))
+                    "#$value"
+                else
+                    value
+
+                try {
+                    currentColor = Color(hexInput)
+                    hexInputError = false
+                } catch(iae: IllegalArgumentException) {
+                    hexInputError = true
+                }
+            },
+            isError = hexInputError
+        )
+
+        // Color picker
         ColorPicker(
             color = currentColor,
             onColorChanged = { color ->
                 colorChanged = true
-                currentColor = color
+                setColor(color)
             }
         )
     }
