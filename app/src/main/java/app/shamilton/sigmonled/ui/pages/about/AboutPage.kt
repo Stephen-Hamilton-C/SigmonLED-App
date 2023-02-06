cbd4251d44d65a5a2825c6c0dcaf095f1ad223bf
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2023-02-06 12:16:24 -0500

    About page uses much simpler method of retrieving versionName

--- app/src/main/java/app/shamilton/sigmonled/ui/pages/about/AboutPage.kt
+++ app/src/main/java/app/shamilton/sigmonled/ui/pages/about/AboutPage.kt
@@ -1,33 +1,22 @@
 package app.shamilton.sigmonled.ui.pages.about
 
-import android.content.pm.PackageManager
-import android.os.Build
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextDecoration
+import app.shamilton.sigmonled.BuildConfig
 import app.shamilton.sigmonled.ui.theme.defaultTextStyle
 
 @Composable
 fun AboutPage(modifier: Modifier) {
     Column(modifier = modifier) {
-        // Get packageInfo for versionName
-        // Source: https://stackoverflow.com/a/6593822
-        val context = LocalContext.current
-        val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            context.packageManager.getPackageInfo(context.packageName, PackageManager.PackageInfoFlags.of(0))
-        } else {
-            context.packageManager.getPackageInfo(context.packageName, 0)
-        }
-
         // Hyperlink
         // Source: https://stackoverflow.com/a/65656351
         val uriHandler = LocalUriHandler.current
@@ -57,7 +46,7 @@ fun AboutPage(modifier: Modifier) {
 
         // TODO: Show app icon
         Text("SigmonLED")
-        Text("Version ${packageInfo.versionName}")
+        Text("Version ${BuildConfig.VERSION_NAME}")
         Text("Written by Stephen-Hamilton-C")
         ClickableText(
             text = annotatedLinkString,
